917ae8fd799ae1c11e3f268825f201a9022de3da
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-08-21 17:55:04 -0300

    Button.tsx
    adicionando novos metodos na classe

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,44 @@ import { Component } from "react";
 
 interface ButtonProps {
   onClick?: () => any;
+  initialiazeClicked?: boolean;
 }
 
-class Button extends Component<ButtonProps> {
+interface ButtonState {
+  alreadyClicked: boolean;
+  timerId?: NodeJS.Timeout;
+}
+
+class Button extends Component<ButtonProps, ButtonState> {
+  constructor(props: ButtonProps) {
+    super(props);
+
+    this.state = {
+      alreadyClicked: !!props.initialiazeClicked,
+    };
+  }
+
+  componentDidMount(): void {
+    const timerId = setInterval(() => {}, 5000);
+    this.setState({ timerId });
+  }
+
+  componentWillUnmount(): void {
+    if (this.state.timerId) clearInterval(this.state.timerId);
+  }
+
   render() {
-    return <button onClick={this.props.onClick}>ver mais</button>;
+    return (
+      <button
+        onClick={() => {
+          this.setState({ alreadyClicked: true });
+          this.props.onClick?.call([]);
+        }}
+        disabled={this.state.alreadyClicked}
+      ></button>
+    );
   }
 }
 
 export default Button;
+
