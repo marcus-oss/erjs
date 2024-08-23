58c776542fb93013aadf144e378f4ad4d6ee779b
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-08-23 17:00:30 -0300

    App.tsx
    estudo sobre listas no projeto , como renderizar uma lista no react

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import "./App.css";
-import React, { useState } from "react";
-import Post, { PostObject } from "./components/Post";
-import Card from "./components/Card";
 
 function App() {
-  const [post, setPost] = useState<PostObject | undefined>({
-    title: "Titulo maneiro",
-    content: "era para ter algo relavante aqui",
-  });
-
+  const name = ["Usuario", "Usuario2 ", "Usuario 3", "usuario 2"];
   return (
     <div className="App">
       <header className="App-header">
-        {!!post && (
-          <Card align="left" title="card">
-            <Post post={post} totalComments={23} />
-          </Card>
-        )}
-
-        <button
-          onClick={() => {
-            setPost(undefined);
-          }}
-        >
-          Remover post
-        </button>
+        <ul>
+          {name.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
